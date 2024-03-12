4c597d29bcfaf57817b1bf35ffcb04a6335e7e92
degreatjoe <jgreat770@gmail.com>
2024-03-12 12:21:40 +0100

    the last digit of a number.

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+def print_last_digit(number):
+    # Obtain the last digit of the number by using the modulo operator
+    last_digit = abs(number) % 10
+    
+    # Print the last digit
+    print(last_digit)
+    
+    # Return the last digit
+    return last_digit
