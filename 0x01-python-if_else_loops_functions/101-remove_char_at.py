3193361a52e0bbbf07b9517751e9012080e7dd43
degreatjoe <jgreat770@gmail.com>
2024-03-13 12:45:00 +0100

    fixes

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def remove_char_at(input_str, n):
-    if n < 0 or n >= len(input_str):
-        return "Invalid index"
-    else:
         return input_str[:n] + input_str[n+1:]
