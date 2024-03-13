ba518c05b3585a258af4590309cbcc4b7d4fb95b
degreatjoe <jgreat770@gmail.com>
2024-03-13 12:37:35 +0100

    fixes

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,7 +4,7 @@ def print_last_digit(number):
     last_digit = abs(number) % 10
     
     # Print the last digit
-    print(last_digit)
+    # print(last_digit)
     
     # Return the last digit
     return last_digit
