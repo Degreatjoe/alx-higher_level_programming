f090d49347469d505737597d74fc2fc6703375f8
degreatjoe <jgreat770@gmail.com>
2024-03-14 15:10:08 +0100

    fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
 # Importing the add function from add_0.py
-from add_0 import add
+    from add_0 import add
 # Assigning values to variables a and b
-a = 1
-b = 2
+    a = 1
+    b = 2
 # Printing the result of the addition
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
