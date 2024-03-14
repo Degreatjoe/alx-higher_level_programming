a48c19a69a58bab1589a034783a87b33b01470e1
degreatjoe <jgreat770@gmail.com>
2024-03-14 16:18:45 +0100

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
+import sys
 
 
-    def main():
-        args = sys.argv[1:]
-        num_args = len(args)
+def main():
+    args = sys.argv[1:]
+    num_args = len(args)
 
-        if num_args == 0:
-            print("{} arguments.".format(num_args))
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    else:
+        if num_args == 1:
+            print("{} argument:".format(num_args))
         else:
-            if num_args == 1:
-                print("{} argument:".format(num_args))
-            else:
-                print(f"{num_args} arguments:")
+            print(f"{num_args} arguments:")
 
-            for i in range(num_args):
-                print(f"{i + 1}: {args[i]}")
+        for i in range(num_args):
+            print(f"{i + 1}: {args[i]}")
 
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
