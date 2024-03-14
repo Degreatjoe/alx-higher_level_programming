ad7e46059b200c5782f2acb021a11051075416e0
degreatjoe <jgreat770@gmail.com>
2024-03-14 12:23:08 +0100

    variable_loadf

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import variable_load_5
+
+
+def main():
+    # Access the variable 'a' from the imported module and print its value
+    print(variable_load_5.a)
+
+
+if __name__ == "__main__":
+    main()
+    
