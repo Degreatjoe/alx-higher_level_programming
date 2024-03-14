880ffac51e05354649755116a74fa04520a56b9b
degreatjoe <jgreat770@gmail.com>
2024-03-14 10:01:26 +0100

    modified for pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+# Importing the add function from add_0.py
+from add_0 import add
 # Assigning values to variables a and b
 a = 1
 b = 2
-
-# Importing the add function from add_0.py
-from add_0 import add
-
 # Printing the result of the addition
 print("{} + {} = {}".format(a, b, add(a, b)))
