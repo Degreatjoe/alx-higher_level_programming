af2d492dd2bd94eafec5d318961acdf1d096f4c3
degreatjoe <jgreat770@gmail.com>
2024-03-14 11:09:06 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    else:
+        if num_args == 1:
+            print("{} argument:".format(num_args))
+        else:
+            print(f"{num_args} arguments:")
+
+        for i in range(num_args):
+            print(f"{i + 1}: {args[i]}")
+
+if __name__ == "__main__":
+    main()
