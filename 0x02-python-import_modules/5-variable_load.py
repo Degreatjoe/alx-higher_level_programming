1f9f8c887f70a854cee046f0a5dad4df37659dcd
degreatjoe <jgreat770@gmail.com>
2024-03-15 01:03:46 +0100

    fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import variable_load_5
 
-#  if __name__ == "__main__":
-print(variable_load_5.a)
+def main():
+    print(variable_load_5.a)
+if __name__ == "__main__":
+    main()
