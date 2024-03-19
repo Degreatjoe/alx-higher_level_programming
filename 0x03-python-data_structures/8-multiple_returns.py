762086e6e10bbbe162384a8282ca0becd0b22a58
degreatjoe <jgreat770@gmail.com>
2024-03-19 10:11:36 +0100

    8-multiple_returns_fixes

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence is None:
-        return None, char
+    if length == 0:
+        char = None
+        return 0, char
     else:
         char = sentence[0]
         return length, char
