9c2740a5d2bca135f84bf2fa68828b7516981970
degreatjoe <jgreat770@gmail.com>
2024-04-17 13:35:01 +0100

    10-square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Defining a square'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class representation of a square'''
+    def __init__(self, size):
+        '''An instantiation with size
+        Args:
+            size (init): the size of the square
+            '''
+        self.__size = 0
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+        def __str__(self):
+            """Return the string representation of the square."""
+            return "[Square] {}/{}".format(self.__size, self.__size)
