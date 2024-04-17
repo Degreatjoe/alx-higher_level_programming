459d13363571c1928a9abac292aacc6422a3bc9d
degreatjoe <jgreat770@gmail.com>
2024-04-17 14:33:44 +0100

    0-read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,3 @@ def read_file(filename=""):
     with open(filename, "r+", encoding="utf-8") as file:
         content = file.read()  # Read the content
         print(content)
-
-read_file('AUTHORS')
