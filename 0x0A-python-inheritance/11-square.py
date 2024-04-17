b365700543a7abcb0d0f69f85731f11010df2986
degreatjoe <jgreat770@gmail.com>
2024-04-17 13:47:40 +0100

    11-square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,10 +17,6 @@ class Square(Rectangle):
         self.__size = size
         super().__init__(size, size)
 
-        def area(self):
-            """Compute the area of the rectangle."""
-            return self.__width * self.__height
-
         def __str__(self):
             """Return the string representation of the square."""
             return "[Square] {}/{}".format(self.__size, self.__size)
