22a48cc15991f7e0dcb16887a0d0a43e938f54d3
degreatjoe <jgreat770@gmail.com>
2024-04-17 11:35:52 +0100

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
-'''Defining my list class that inherits from list'''
-
-
 class MyList(list):
-    '''myList inherits from list '''
+    '''MyList inherits from list '''
+    ERR_MSG = "'<' not supported between instances of 'str' and 'int'"
+
+    def __init__(self, *args):
+        '''Initializes the list and checks if all elements are integers'''
+        super().__init__(*args)
+        if not all(isinstance(x, int) for x in self):
+            raise TypeError(MyList.ERR_MSG)
 
     def print_sorted(self):
         '''Prints the sorted list'''
