7ee2da30c51e86457fdc113a9037df977ee841ff
Great~ <jgreat779@gmail.com>
2024-06-26 16:34:14 +0100

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,8 +6,9 @@ class Square extends SquareBase {
   charPrint (c) {
     const char = c === undefined ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.widdth));
+      console.log(char.repeat(this.width));
     }
   }
 }
+
 module.exports = Square;
