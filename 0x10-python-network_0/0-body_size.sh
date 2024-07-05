746efeaabe35afb05c521a03f91a19374afdf605
great <jgreat770@gmail.com>
2024-07-04 21:55:46 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,23 +1,14 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# Function to send request and display size of response body
-get_response_size() {
-    local url="$1"
-    local response_size=$(curl -sI "$url" | grep -i '^Content-Length:' | awk '{print $2}')
-    
-    if [[ -n "$response_size" ]]; then
-        echo "$response_size"
-    else
-        echo "Error: Unable to retrieve content length."
-    fi
-}
-
-# Main script starts here
-if [[ $# -ne 1 ]]; then
+# Check if URL argument is provided
+if [ -z "$1" ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
 
-url="$1"
-get_response_size "$url"
+# Retrieve the URL and get the size of the response body in bytes
+response_size=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r')
+
+# Print the size of the response body
+echo "$response_size"
 
