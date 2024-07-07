e6aa4e35fe3e02777bc2de0e341b74f4e6a4da3d
Great~ <jgreat779@gmail.com>
2024-07-07 11:31:14 +0100

    task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
-# Function to send GET request and display body of 200 status code response
-get_url_body() {
-    local url="$1"
-    local response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-    
-    if [[ $response -eq 200 ]]; then
-        curl -s "$url"
-    else
-        echo "Error: Received HTTP status code $response"
-    fi
-}
-
-# Main script starts here
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-get_url_body "$url"
-
+# Sends a GET request to the URL and displays the body of a 200 status code response
+curl -sL -X GET "$1"
