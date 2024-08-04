5ee03f751ee4b191c923df3a888768aff0746c2b
great <jgreat770@gmail.com>
2024-08-04 15:45:28 +0100

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,33 @@ const request = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(count);
-  }
+function countFilmsWithCharacter(url, callback) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    let count = 0;
+
+    data.results.forEach(film => {
+      if (film.characters.some(char => char.endsWith(`/${characterId}/`))) {
+        count++;
+      }
+    });
+
+    // If there is a next page, recurse to handle pagination
+    if (data.next) {
+      countFilmsWithCharacter(data.next, (nextCount) => {
+        callback(count + nextCount);
+      });
+    } else {
+      callback(count);
+    }
+  });
+}
+
+countFilmsWithCharacter(apiUrl, (totalCount) => {
+  console.log(totalCount);
 });
